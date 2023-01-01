d35adce2e4005fcb97b97f12e5f948c1733d68c9
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-01 17:50:24 -0300

    feat: change workflow

---
+++ mobile/src/pages/WorshipDays.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export function WorshipDays(){
+    return (
+        <View>
+            <Text>Domingo:</Text>
+            <Text>17h</Text>
+            <Text>Quarta:</Text>
+            <Text>19h15</Text>
+        </View>
+    );
+}
